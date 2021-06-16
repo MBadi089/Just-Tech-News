79731d249997e3e5d5cae3f326b8723f7825213c
Marcos <marcosbadillo089@gmail.com>
2021-06-16 11:23:04 -0500

    updated code

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+
+const userRoutes = require('./api/user-routes');
+const postRoutes = require('./api/post-routes');
+const commentRoutes = require('./api/comment-route');
+const homeRoutes = require('./home-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+router.use('/', homeRoutes);
+
+module.exports = router;
